804f709e94de808d3fb883b0613ed954af548c78
Jakedruth@gmail.com <Jacob.ruth@mymail.champlain.edu>
2019-01-22 12:33:15 -0500

    First Push with base unity project with some helper classes

---
+++ Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+
+    [MinMaxRange(0,10)]
+    public RangedFloat range;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        if (instance != null)
+        {
+            GameObject.Destroy(gameObject);
+            return;
+        }
+
+        instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+}
