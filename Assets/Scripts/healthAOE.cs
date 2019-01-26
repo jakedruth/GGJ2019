1675e9a957a5cd813f7111e168b3b122f13cf9b1
Evan <askme>
2019-01-25 23:02:53 -0500

    healthAOE

---
+++ Assets/Scripts/healthAOE.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class healthAOE : MonoBehaviour
+{
+
+    public float damage_dealt = 0.0f;
+    public bool affects_player = false;
+    public bool affects_enemy = false;
+    private List<Entity> affects_list;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        foreach (Entity entity in affects_list)
+        {
+            if(entity.isPlayer & affects_player)
+            {
+                entity.DealDamage(GameManager.GameTime * damage_dealt);
+            }
+
+            if(!entity.isPlayer & affects_enemy)
+            {
+                entity.DealDamage(GameManager.GameTime * damage_dealt);
+            }
+        }
+       
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        affects_list.Add(collision.GetComponent<Entity>());
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+
+        affects_list.Remove(collision.GetComponent<Entity>());
+        
+    }
+}
